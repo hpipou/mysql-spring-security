61c328088a83ef594b190b6b5791e3e09119eed7
hpipou <mr.aoudjit@gmail.com>
2022-09-12 08:25:40 +0200

    Set entity

---
+++ src/main/java/io/mysql/springsecurity/entity/AppUser.java
@@ -0,0 +1,24 @@
+package io.mysql.springsecurity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<AppRole> appRoles=new ArrayList<>();
+}
